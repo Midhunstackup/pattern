f878f5a9bb972947178c2e0e0435561cf93c9baf
Midhun <midhun.stackup2002@gmail.com>
2025-06-10 17:22:30 +0530

    arrays

---
+++ arrays.js
@@ -0,0 +1,27 @@
+//Q1: Find the largest number in a 1D array
+/*
+let arr = [4,1,5,3,2];
+let largest = arr[0];
+
+for(let i = 1;i< 6;i++){
+    if (arr[i]>largest){
+        largest=arr[i];   
+    }
+}
+console.log(largest);
+*/
+
+//Q2: Find the smallest element
+/*
+let arr = [4,1,5,3,2,1,10,0];
+let smallest = arr[0];
+
+for(let i = 1;i< 8;i++){
+    if (arr[i]<smallest){
+        smallest=arr[i];   
+    }
+}
+console.log(smallest);
+*/
+
+//Q3: Find the second largest element
